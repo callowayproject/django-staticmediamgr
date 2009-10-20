76a582d23a3b3ca9117994d816f2857669279c44
Corey Oordt <coordt@washingtontimes.com>
2009-10-20 10:11:52 -0400

    Actually put content in the the License and setup.py files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='django-staticmediamgr',
+      version='0.1',
+      description='A Django app to copy static media files over to a remote place, optionally minifying css and js.',
+      long_description='',
+      author='Corey Oordt, The Washington Times',
+      author_email='coordt@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/django-staticmediamgr/',
+      packages=['staticmediamgr'],
+      classifiers=[],
+)
