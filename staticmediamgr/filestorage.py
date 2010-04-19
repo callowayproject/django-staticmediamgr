58503988fbc454b7864d03d7d850e2fca736a13b
Corey Oordt <coordt@washingtontimes.com>
2010-04-19 13:45:23 -0400

    typo

--- staticmediamgr/filestorage.py
+++ staticmediamgr/filestorage.py
@@ -15,17 +15,16 @@ class StaticMediaStorage(FileSystemStorage):
     
     To use this by default, in your settings.py ::
     
-    from staticmediamgr.storage import StaticMediaStorage
-    DEFAULT_FILE_STORAGE = StaticMediaStorage()
+    DEFAULT_FILE_STORAGE = 'staticmediamgr.storage.StaticMediaStorage'
     
     For FileFields and ImageFields, you can pass a relative location and it
     will store your uploaded files under that combined path. For example::
     
     photo = ImageField(storage=StaticMediaStorage(location='photos/%Y/%b/%d/'))
     
-    The uploaded files would be stored under STATIC_ROOT/photos/%Y/%b/%d/
+    The uploaded files would be stored under ``STATIC_ROOT/photos/%Y/%b/%d/``
     """
     def __init__(self, location='', base_url='', *args, **kwargs):
         real_location = os.path.join(DEFAULT_ROOT, location)
-        real_base_url = DEFAULT_URL + base
+        real_base_url = DEFAULT_URL + base_url
         super(StaticFileStorage, self).__init__(real_location, real_base_url, *args, **kwargs)
